9f734e328f714d1faf74c5cb560718e4dd08b307
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2020-02-17 22:46:11 -0800

    .

--- src/main/java/skylorbeck/extraarmor/extraarmor_main.java
+++ src/main/java/skylorbeck/extraarmor/extraarmor_main.java
@@ -1,4 +1,23 @@
 package skylorbeck.extraarmor;
 
+import net.minecraft.item.Item;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
 public class extraarmor_main {
+    public extraarmor_main(){
+        {
+            MinecraftForge.EVENT_BUS.register(this);
+        }
+    }
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegEvents {
+        @SubscribeEvent
+        public static void onItemRegistery(RegistryEvent.Register<Item> event) {
+            event.getRegistry().registerAll(
+            );
+        }
+    }
 }
---
+++ src/main/java/skylorbeck/extraarmor/extraarmor_materials.java
@@ -0,0 +1,66 @@
+package skylorbeck.extraarmor;
+
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyLoadBase;
+import net.minecraft.util.SoundEvent;
+
+import java.util.function.Supplier;
+
+public class extraarmor_materials implements IArmorMaterial {
+    
+
+    private static final int[] MAX_DAMAGE_ARRAY = new int[]{13, 15, 16, 11};
+    private final String name;
+    private final int maxDamageFactor;
+    private final int[] damageReductionAmountArray;
+    private final int enchantability;
+    private final SoundEvent soundEvent;
+    private final float toughness;
+    private final LazyLoadBase<Ingredient> repairMaterial;
+
+    private extraarmor_materials(String nameIn, int maxDamageFactorIn, int[] damageReductionAmountsIn, int enchantabilityIn, SoundEvent equipSoundIn, float p_i48533_8_, Supplier<Ingredient> repairMaterialSupplier) {
+        this.name = nameIn;
+        this.maxDamageFactor = maxDamageFactorIn;
+        this.damageReductionAmountArray = damageReductionAmountsIn;
+        this.enchantability = enchantabilityIn;
+        this.soundEvent = equipSoundIn;
+        this.toughness = p_i48533_8_;
+        this.repairMaterial = new LazyLoadBase<>(repairMaterialSupplier);
+    }
+    @Override
+    public int getDurability(EquipmentSlotType slotIn) {
+        return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * this.maxDamageFactor;
+    }
+
+    @Override
+    public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+        return this.damageReductionAmountArray[slotIn.getIndex()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public SoundEvent getSoundEvent() {
+        return this.soundEvent;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return this.repairMaterial.getValue();
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.toughness;
+    }
+}
